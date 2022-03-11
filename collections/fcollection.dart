68662913d4da93d5d6b2f71e87ed458872f12449
Ahmad Tariq <ahmadbintariq4u@gmail.com>
2022-03-11 12:30:19 +0500

    Adding FCollection class

---
+++ collections/fcollection.dart
@@ -0,0 +1,17 @@
+class FCollection {
+  /// Create a deme list with specific items
+  List createList({items = 0}) {
+    var list = [];
+    for (var a = 0; a < items; a++) {
+      list.add(a);
+    }
+    return list;
+  }
+
+  /// print the list
+  printList({required list}) {
+    for (var i in list) {
+      print(i);
+    }
+  }
+}
---
+++ collections/readme.md
@@ -0,0 +1,5 @@
+# FCollections 
+
+Following functions are available in this class
+1. createList({items=0})   
+> Return a list from 0 until your items.
